353fe1f587f9b03ca3fb78e83f45ec16315369c1
Michał Bielawski <michal.bielawski@apperio.com>
2017-11-09 20:22:07 +0000

    Added a failing test for #5582

--- tests/test_middleware.py
+++ tests/test_middleware.py
@@ -4,15 +4,23 @@ from django.test import override_settings
 
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
 from rest_framework.test import APITestCase
 from rest_framework.views import APIView
 
+
+class PostView(APIView):
+    def post(self, request):
+        return Response(data=request.data, status=200)
+
+
 urlpatterns = [
-    url(r'^$', APIView.as_view(authentication_classes=(TokenAuthentication,))),
+    url(r'^auth$', APIView.as_view(authentication_classes=(TokenAuthentication,))),
+    url(r'^post$', PostView.as_view()),
 ]
 
 
-class MyMiddleware(object):
+class RequestUserMiddleware(object):
     def __init__(self, get_response):
         self.get_response = get_response
 
@@ -24,13 +32,30 @@ class MyMiddleware(object):
         return response
 
 
+class RequestPOSTMiddleware(object):
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        request.body
+        response = self.get_response(request)
+        assert request.POST['foo'] == 'bar'
+
+        return response
+
+
 @override_settings(ROOT_URLCONF='tests.test_middleware')
 class TestMiddleware(APITestCase):
 
-    @override_settings(MIDDLEWARE=('tests.test_middleware.MyMiddleware',))
+    @override_settings(MIDDLEWARE=('tests.test_middleware.RequestUserMiddleware',))
     def test_middleware_can_access_user_when_processing_response(self):
         user = User.objects.create_user('john', 'john@example.com', 'password')
         key = 'abcd1234'
         Token.objects.create(key=key, user=user)
 
         self.client.get('/auth', HTTP_AUTHORIZATION='Token %s' % key)
+
+    @override_settings(MIDDLEWARE=('tests.test_middleware.RequestPOSTMiddleware',))
+    def test_middleware_can_access_request_post_when_processing_response(self):
+        response = self.client.post('/post', {'foo': 'bar'})
+        assert response.status_code == 200
