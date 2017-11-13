7583b3a34c8aa0731d06067f076d949bb4b43a56
Ryan P Kilby <rpkilby@ncsu.edu>
2017-11-13 15:10:31 -0500

    Set data ref on underlying django request

--- rest_framework/request.py
+++ rest_framework/request.py
@@ -250,8 +250,9 @@ class Request(object):
             else:
                 self._full_data = self._data
 
-            # copy files refs to the underlying request so that closable
+            # copy data & files refs to the underlying request so that closable
             # objects are handled appropriately.
+            self._request._post = self._data
             self._request._files = self._files
 
     def _load_stream(self):
