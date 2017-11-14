aa6a4c4941f708a89d46f83bb6ee09d0eb5c8ef1
Michał Bielawski <michal.bielawski@apperio.com>
2017-11-14 10:05:36 +0000

    Added a test to show the interface on Django's request.POST changes for JSON requests.

--- tests/test_middleware.py
+++ tests/test_middleware.py
@@ -1,11 +1,13 @@
 from django.conf.urls import url
 from django.contrib.auth.models import User
+from django.http import HttpResponse
 from django.test import override_settings
 
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from rest_framework.test import APITestCase
+from rest_framework.utils import json
 from rest_framework.views import APIView
 
 
@@ -14,9 +16,14 @@ class PostView(APIView):
         return Response(data=request.data, status=200)
 
 
+def regular_view(request):
+    return HttpResponse()
+
+
 urlpatterns = [
     url(r'^auth$', APIView.as_view(authentication_classes=(TokenAuthentication,))),
     url(r'^post$', PostView.as_view()),
+    url(r'^regular$', regular_view),
 ]
 
 
@@ -44,6 +51,17 @@ class RequestPOSTMiddleware(object):
         return response
 
 
+class RequestJSONMiddleware(object):
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        assert list(request.POST.keys()) == []
+
+        return response
+
+
 @override_settings(ROOT_URLCONF='tests.test_middleware')
 class TestMiddleware(APITestCase):
 
@@ -59,3 +77,13 @@ class TestMiddleware(APITestCase):
     def test_middleware_can_access_request_post_when_processing_response(self):
         response = self.client.post('/post', {'foo': 'bar'})
         assert response.status_code == 200
+
+    @override_settings(MIDDLEWARE=('tests.test_middleware.RequestJSONMiddleware',))
+    def test_original_request_post_is_not_populated_on_json_requests(self):
+        # regular view
+        response = self.client.post('/regular', json.dumps({'foo': 'bar'}), content_type='application/json')
+        assert response.status_code == 200
+
+        # drf
+        with self.assertRaises(AssertionError):
+            response = self.client.post('/post', json.dumps({'foo': 'bar'}), content_type='application/json')
